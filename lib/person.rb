47aec5df61e8e4e12940af8fb3a00fffa925d9c0
Chely Ho <adept_inept@yahoo.com>
2018-02-05 04:49:04 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,3 @@
-class Dog
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+class Person
 
 end
