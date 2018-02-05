199c60bbbbc668456a98e9df8c800a2d133d43b7
Chely Ho <adept_inept@yahoo.com>
2018-02-05 04:46:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,12 @@
 class Dog
-  def initialize(name)
-    dog_name = name
+
+  
+
+  def name=(name)
+    @name = name
   end
 
   def name
     @name = name
-  end
-
-  def name=(new_name)
-    @name = new_name
   end
 end
